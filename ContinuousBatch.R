a02052c5fc4a8be8496f97f86ad4e0b64e94c8fe
hkorotkin <hkorotki@vols.utk.edu>
2016-03-10 13:14:37 -0500

    everything works, but not sure if everything is correct...

--- ContinuousBatch.R
+++ ContinuousBatch.R
@@ -67,7 +67,7 @@ print(nodeBased.OUMV)
 #Warning with this?
 
 #Now run all OUwie models:
-models <- c("BM1","BMS","OU1","OUMV","OUMA","OUMVA") ##OUM not working, man
+models <- c("BM1","BMS","OU1","OUMV","OUMA","OUMVA") ##OUM not working
 results <- lapply(models, RunSingleOUwieModel, phy=labeled.tree, data=cleaned.continuous2)
 
 
@@ -89,25 +89,22 @@ print(best) #prints info on best model
 ?OUwie.fixed
 
 #Next, keep all parameters but alpha at their maximum likelihood estimates (better would be to fix just alpha and let the others optimize given this constraint, but this is harder to program for this class). Try a range of alpha values and plot the likelihood against this.
-alpha.values<-seq(from=, to=, length.out=50)
-stop("replace the _______________ and delete this stop")
+alpha.values<-seq(from=0.1, to=1, length.out=50)
 
 #keep it simple (and slow) and do a for loop:
 likelihood.values <- rep(NA, length(alpha.values))
 for (iteration in sequence(length(alpha.values))) {
-	likelihood.values[iteration] <- OUwie.fixed(tree, trait, model="OUMV", alpha=rep(alpha.values[iteration],2), sigma.sq=best$solution[2,], theta=best$theta[,1])$loglik
+	likelihood.values[iteration] <- OUwie.fixed(labeled.tree, data=cleaned.continuous2, model="OUMVA", alpha=rep(alpha.values[iteration]), sigma.sq=best$solution[2,], theta=best$theta[,1])$loglik
 }
 
-plot(x= _______________ , y= _______________, xlab="_______________", ylab="_______________", type="l", bty="n")
-stop("replace the _______________ and delete this stop")
+plot(x=alpha.values, y=likelihood.values, xlab="Aplha", ylab="Log Likelihood", type="l", bty="n")
 
 
 points(x=best$solution[1,1], y=best$loglik, pch=16, col="red")
 text(x=best$solution[1,1], y=best$loglik, "unconstrained best", pos=4, col="red")
 
 #a rule of thumb for confidence for likelihood is all points two log likelihood units worse than the best value. Draw a dotted line on the plot to show this
-abline(h=_______________, lty="dotted") #Two log-likelihood 
-stop("replace the _______________ and delete this stop")
+abline(h=best$loglik-2, lty="dotted") #Two log-likelihood 
 
 
 #Now, let's try looking at both theta parameters at once, keeping the other parameters at their MLEs
@@ -119,7 +116,7 @@ theta2.points<-c(best$theta[2,1], rnorm(nreps-1, best$theta[2,1], 5*best$theta[2
 likelihood.values<-rep(NA,nreps)
 
 for (iteration in sequence(nreps)) {
-	likelihood.values[iteration] <- OUwie.fixed(tree, trait, model="OUMV", alpha=best$solution[1,], sigma.sq=best$solution[2,], theta=c(theta1.points[iteration], theta2.points[iteration]))$loglik
+	likelihood.values[iteration] <- OUwie.fixed(labeled.tree, cleaned.continuous2, model="OUMVA", alpha=best$solution[1,], sigma.sq=best$solution[2,], theta=c(theta1.points[iteration], theta2.points[iteration]))$loglik
 }
 #think of how long that took to do 400 iterations. Now remember how long the search took (longer).
 
@@ -132,8 +129,8 @@ contour(interpolated.points, xlim=range(c(theta1.points, theta2.points)),ylim=ra
 
 points(x=best$theta[1,1], y=best$theta[2,1], col="red", pch=16)
 
-points(x=trait$X[which(trait$Reg==1)],y=rep(min(c(theta1.points, theta2.points)), length(which(trait$Reg==1))), pch=18, col=rgb(0,0,0,.3)) #the tip values in regime 1, plotted along x axis
-points(y=trait$X[which(trait$Reg==2)],x=rep(min(c(theta1.points, theta2.points)), length(which(trait$Reg==2))), pch=18, col=rgb(0,0,0,.3)) #the tip values in regime 2, plotted along y axis
+points(x=cleaned.continuous2$X[which(cleaned.continuous2$Reg==1)],y=rep(min(c(theta1.points, theta2.points)), length(which(cleaned.continuous2$Reg==1))), pch=18, col=rgb(0,0,0,.3)) #the tip values in regime 1, plotted along x axis
+points(y=cleaned.continuous2$X[which(cleaned.continuous2$Reg==2)],x=rep(min(c(theta1.points, theta2.points)), length(which(cleaned.continuous2$Reg==2))), pch=18, col=rgb(0,0,0,.3)) #the tip values in regime 2, plotted along y axis
 
 
 #The below only works if the discrete trait rate is low, so you have a good chance of estimating where the state is.
@@ -141,16 +138,16 @@ points(y=trait$X[which(trait$Reg==2)],x=rep(min(c(theta1.points, theta2.points))
 #regime 2 vice versa. This makes the models more similar than they should be.
 #See Revell 2013, DOI:10.1093/sysbio/sys084 for an exploration of this effect.
 library(phytools)
-trait.ordered<-data.frame(trait[,2], trait[,2],row.names=trait[,1])
+trait.ordered<-data.frame(cleaned.continuous2[,2], cleaned.continuous2[,2],row.names=cleaned.continuous2[,1])
 trait.ordered<- trait.ordered[tree$tip.label,]
 z<-trait.ordered[,1]
 names(z)<-rownames(trait.ordered)
-tree.mapped<-make.simmap(tree,z,model="ER",nsim=1)
+tree.mapped<-make.simmap(labeled.tree,z,model="ER",nsim=1)
 leg<-c("black","red")
 names(leg)<-c(1,2)
 plotSimmap(tree.mapped,leg,pts=FALSE,ftype="off", lwd=1)
 
-simmapBased<-OUwie(tree.mapped,trait,model="OUMV", simmap.tree=TRUE, diagn=FALSE)
+simmapBased<-OUwie(tree.mapped,cleaned.continuous2,model="OUMVA", simmap.tree=TRUE, diagn=FALSE)
 print(simmapBased)
 #How does this compare to our best model from above? Should they be directly comparable?
 print(best)
